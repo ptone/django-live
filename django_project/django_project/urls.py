ef2d889d9d11596c13bab578faa070b6ebcc1850
Preston Holmes <preston@ptone.com>
2012-08-31 06:35:43 -0700

    I don't always serve static in production
    but when I do, I use 'static.serve' :=P

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -24,8 +26,8 @@ urlpatterns = patterns('',
     url(r'^colors/', include('colorpicks.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT}),
+)
 
