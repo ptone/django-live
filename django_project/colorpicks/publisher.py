820763bee901856a2278d09075070ccaa7af529f
Preston Holmes <preston@ptone.com>
2012-09-03 07:42:20 -0700

    annotate and cleanup publisher code

--- django_project/colorpicks/publisher.py
+++ django_project/colorpicks/publisher.py
@@ -10,12 +10,50 @@ from predicate import P
 
 from colorpicks.models import ColorChoice
 
+"""
+This module has three parts and should probably eventually be split up
+
+defines a "Collection" class
+
+defines a global 'collections' dictionary to hold collection instances
+
+connects to ORM signals for model save/delete
+
+creates a few app-wide common collections
+"""
+
 redis_client = Redis(connection_pool=settings.REDIS_POOL)
 
 # reset current users at server startup
 redis_client.delete('connected_users')
 
 class Collection(object):
+    """
+    An in memory representation of a group of objects
+
+    This class manages relaying ORM signal events to any connected socket
+    clients by managing a couple redis structures.
+
+    A collection consists of:
+
+        name/label
+        predicate
+        redis set
+        redis channel
+
+    A predicate is essentially like a Django Q object, be can be used to evaluate
+    whether an object meets a condition, without running a query against the DB
+
+    After initially setting up the Collection - any save/delete model signal
+    will relay to all collections .update method - which checks whether the model
+    matches the predicate, if is already a member, and adds or removes it to the
+    collection as appropriate. Any add/remove is broadcast to anyone subscribed
+    to the collection's channel.
+
+    Currently there are aspects of this class that are highly coupled to this
+    demo - but it could be refactored and made a bit more abstract without much
+    effort.
+    """
 
     def __init__(self, name, predicate):
         self.name = name
@@ -23,52 +61,40 @@ class Collection(object):
         self.reset()
 
     def reset(self):
-        # print 'collection reset for ', self.name
+        """
+        Clear/Set up our redis set
+        Query the db for any current members
+        Update collection with current db members
+        """
+
         redis_client.delete(self.name)
         current_members = ColorChoice.objects.filter(self.predicate)
-        # print current_members.count(), " current members"
         for member in current_members:
             self.update(member)
-        # current_ids = set([str(m.id) for m in current_members])
-        # print 'current ids ', current_ids
-        # redis_members = redis_client.smembers(self.name)
-        # print 'redis members', redis_members
-        # removed_ids = redis_members - current_ids
-        # print 'removing ids ', removed_ids
-        # if len(removed_ids):
-            # removed_objects = ColorChoice.objects.filter(
-                    # id__in=[int(i) for i in removed_ids])
-            # # print removed_objects
-            # for obj in removed_objects:
-                # self.remove(obj)
-            # # remove any stale IDs of items no in the DB
-            # redis_client.srem(self.name, *tuple(removed_ids))
 
     def add(self, instance):
-        print 'adding ', instance, self.name
+        print 'evaluating for add ', instance, self.name
         if not redis_client.sismember(self.name, instance.id):
             # Not a member, add and publish
             redis_client.sadd(self.name, instance.id)
             data = instance.data()
             data['id'] = instance.id
-            # print 'publishing to collection'
             redis_client.publish(self.name, json.dumps(
                 {'action':'create', 'data':data}))
-            print 'added ', instance
+            print 'added'
+        else:
+            print 'not added'
 
 
     def update(self, instance):
         print "updating for ", self.name, instance
-        # new versions of django-predicate allow for an __in__ test
-        if self.predicate.eval(instance):
-            # print 'adding'
+        if instance in self.predicate:
             self.add(instance)
         else:
-            # print 'removing'
             self.remove(instance)
 
     def remove(self, instance):
-        print 'removing', instance, self.name
+        print 'evaluating for removal ', instance, self.name
         if redis_client.sismember(self.name, instance.id):
             # should no longer be a member, remove
             redis_client.srem(self.name, instance.id)
@@ -76,9 +102,9 @@ class Collection(object):
             data['id'] = instance.id
             redis_client.publish(self.name, json.dumps(
                 {'action':'delete', 'data':data}))
-            print 'removed ', instance
+            print 'removed'
         else:
-            print 'no removed'
+            print 'not removed'
 
     def fetch(self):
         data = list(
@@ -88,43 +114,6 @@ class Collection(object):
                     'color_choice'
                     ))
         return data
-
-collections = {}
-
-try:
-    blue_colors = P(
-        hue__range=(171, 264),
-        saturation__range=(30,100),
-        brightness__range=(30, 100)
-        )
-
-    django1 = P(
-        hue__range=(154, 160),
-        saturation__range=(35, 82),
-        brightness__range=(10, 38)
-        )
-
-    django2 = P(
-        hue__range=(82, 95),
-        saturation__range=(70,90),
-        brightness__range=(29,90)
-        )
-
-    django3 = P(
-        hue__range=(131, 148),
-        saturation__range=(20,50),
-        brightness__range=(15,60)
-        )
-
-    django_colors = django1 | django2 | django3
-    collections['blue'] = Collection('blue', blue_colors)
-    collections['all'] = Collection('all', P())
-    collections['django'] = Collection('django', django_colors)
-except DatabaseError as exc:
-    # we will get this if syncdb has not run yet
-    from django.db import connection
-    connection._rollback()
-
 
 def publish_color(sender, instance, **kwargs):
     """
@@ -154,4 +143,49 @@ def delete_color(sender, instance, **kwargs):
 post_save.connect(publish_color, ColorChoice)
 post_delete.connect(delete_color, ColorChoice)
 
+# This is a global collections data structure accessed by all connected sockets
+# it intially holds any commonly defined collections, but clients can add their
+# own collections to wire into the signal-collection dispatching. If there were
+# to be many user registered collections, the signal should be converted to a
+# queued task instead of in the req/res cycle.
+collections = {}
+
+try:
+    # We do this here in the try because we can't
+    # access these models on startup if we are running in the syncdb
+    # code. Could import this file in other-than-models.py to avoid that
+    blue_colors = P(
+        hue__range=(171, 264),
+        saturation__range=(30,100),
+        brightness__range=(30, 100)
+        )
+
+    django1 = P(
+        hue__range=(154, 160),
+        saturation__range=(35, 82),
+        brightness__range=(10, 38)
+        )
+
+    django2 = P(
+        hue__range=(82, 95),
+        saturation__range=(70,90),
+        brightness__range=(29,90)
+        )
+
+    django3 = P(
+        hue__range=(131, 148),
+        saturation__range=(20,50),
+        brightness__range=(15,60)
+        )
+
+    django_colors = django1 | django2 | django3
+
+    # create the common collections
+    collections['blue'] = Collection('blue', blue_colors)
+    collections['all'] = Collection('all', P())
+    collections['django'] = Collection('django', django_colors)
+except DatabaseError as exc:
+    # we will get this if syncdb has not run yet
+    from django.db import connection
+    connection._rollback()
 
